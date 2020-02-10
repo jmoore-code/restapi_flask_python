ee28e95a384fdf8a1cd1ec5d12a2b9254acf9a77
Jason Moore <jason.richard.moore@gmail.com>
2020-02-09 19:05:33 -0700

    First commit

---
+++ code.py
@@ -0,0 +1,29 @@
+# Video 13- advanced set operations 
+
+art = {"Bob", "Jen", "Rolf", "Charlie"}
+science = {"Bob", "Jen", "Adam", "Anne"}
+
+
+both = art.intersection(science)
+# print(both)
+
+# Video 15- Booleans
+
+# the same as javascript for operators
+
+# python can compare list 
+friends = ["Rolf", "Bob"]
+abroad = ["Rolf", "Bob"]
+
+# print(friends == abroad) will print true
+
+# Video 16 - 
+day_of_week = input("what day of the week is it today?").lower()
+if day_of_week == "monday":
+    print("Have a great start to your week!")
+elif day_of_week == "tuesday":
+    print("It's Tuesday")
+else:
+    print("Full speed ahead!")
+
+print("This always runs") 
